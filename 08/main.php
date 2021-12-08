3037128537bd1918b32720acc02bdd38d938d7c4
bake <bake@192k.pw>
2021-12-08 22:31:21 +0100

    Day 8 small cleanup

--- 08/main.php
+++ 08/main.php
@@ -3,73 +3,59 @@
 function input($handle): iterable
 {
     while ($line = fgets($handle)) {
-        $line = explode(' | ', $line);
-        $signal = array_map('trim', explode(' ', $line[0]));
-        $output = array_map('trim', explode(' ', $line[1]));
+        $nums = explode(' ', $line);
+        $nums = array_map(fn ($num) => str_split(trim($num)), $nums);
+        array_walk($nums, fn (&$num) => sort($num));
+        $signal = array_slice($nums, 0, 10);
+        $output = array_slice($nums, 11, 4);
         yield [$signal, $output];
     }
 }
 
 function part1(array $entries): int
 {
-    return array_sum(array_map(fn ($e) => count(array_filter($e[1], fn ($o) => in_array(strlen($o), [2, 3, 4, 7]),)), $entries,));
+    return array_sum(array_map(fn ($e) => count(array_filter($e[1], fn ($o) => in_array(count($o), [2, 3, 4, 7]),)), $entries,));
 }
 
-// This has to be refactored.
+function array_find(array $array, callable $callback): mixed
+{
+    return array_values(array_filter($array, $callback))[0] ?? null;
+}
+
+function number_intersect(array $a, array $b): int
+{
+    return count(array_intersect($a, $b));
+}
+
 function part2(array $entries): int
 {
-    $length_segment = [
-        2 => 1,
-        3 => 7,
-        4 => 4,
-        7 => 8,
-    ];
     $sum = 0;
     foreach ($entries as [$signal, $output]) {
-        $signal = array_map(function (string $num): string {
-            $num = str_split($num);
-            sort($num);
-            return implode('', $num);
-        }, $signal);
-        $output = array_map(function (string $num): string {
-            $num = str_split($num);
-            sort($num);
-            return implode('', $num);
-        }, $output);
-
-        $test = [];
-        foreach ($signal as $num) {
-            if (!isset($length_segment[strlen($num)])) {
-                continue;
-            }
-            $test[$length_segment[strlen($num)]] = $num;
-        }
+        $test = [
+            1 => array_find($signal, fn ($num) => count($num) === 2),
+            4 => array_find($signal, fn ($num) => count($num) === 4),
+            7 => array_find($signal, fn ($num) => count($num) === 3),
+            8 => array_find($signal, fn ($num) => count($num) === 7),
+        ];
 
         while (count($test) < 10) {
             foreach ($signal as $num) {
-                if (isset($length_segment[strlen($num)])) {
-                    continue;
-                }
-                switch (strlen($num)) {
+                switch (count($num)) {
                     case 5:
-                        if (isset($test[4]) && count(array_intersect(str_split($num), str_split($test[4]))) === 2) {
+                        if (number_intersect($num, $test[4] ?? []) === 2) {
                             $test[2] = $num;
-                        }
-                        if (isset($test[1]) && count(array_intersect(str_split($num), str_split($test[1]))) === 2) {
+                        } else if (number_intersect($num, $test[1] ?? []) === 2) {
                             $test[3] = $num;
-                        }
-                        if (isset($test[2]) && $test[2] !== $num && isset($test[3]) && $test[3] !== $num) {
+                        } else {
                             $test[5] = $num;
                         }
                         break;
                     case 6:
-                        if (isset($test[1]) && count(array_intersect(str_split($num), str_split($test[1]))) === 1) {
+                        if (number_intersect($num, $test[1] ?? []) === 1) {
                             $test[6] = $num;
-                        }
-                        if (isset($test[3]) && count(array_intersect(str_split($num), str_split($test[3]))) === 5) {
+                        } else if (number_intersect($num, $test[3] ?? []) === 5) {
                             $test[9] = $num;
-                        }
-                        if (isset($test[6]) && $test[6] !== $num && isset($test[9]) && $test[9] !== $num) {
+                        } else {
                             $test[0] = $num;
                         }
                         break;
@@ -77,6 +63,8 @@ function part2(array $entries): int
             }
         }
 
+        $test = array_map(fn ($num) => implode('', $num), $test);
+        $output = array_map(fn ($num) => implode('', $num), $output);
         $test = array_flip($test);
         $output = array_map(fn ($num) => $test[$num], $output);
         $sum += (int) implode('', $output);
